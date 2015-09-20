232ab71a826b1eecd0074a614a64d7d2f55a27cd
Adriano Schmidt <adriano@localhost8080.com.br>
2015-09-20 17:30:20 -0300

    initial version

---
+++ src/main/java/br/com/localhost8080/wildflyswarm/App.java
@@ -0,0 +1,9 @@
+package br.com.localhost8080.wildflyswarm;
+
+import org.wildfly.swarm.container.Container;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		Container container = new Container();
+	}
+}
